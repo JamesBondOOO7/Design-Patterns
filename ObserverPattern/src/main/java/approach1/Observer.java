6cb4cef363d78665728aff394a5542513f34b6a5
Manan Arora <mananaroramail@gmail.com>
2022-09-27 01:28:34 +0530

    Chapter 2

--- ObserverPattern/src/main/java/approach1/Observer.java
+++ ObserverPattern/src/main/java/approach1/Observer.java
@@ -1,7 +1,7 @@
 package approach1;
 
 public interface Observer {
-    // (temp, humidity, pressure) :: state values the approach1.Observer
-    // gets from the approach1.Subject when a weather measurement changes
+    // (temp, humidity, pressure) :: state values the Observer
+    // gets from the Subject when a weather measurement changes
     public void update(float temperature, float humidity, float pressure);
 }
--- ObserverPattern/src/main/java/approach1/Subject.java
+++ ObserverPattern/src/main/java/approach1/Subject.java
@@ -1,11 +1,11 @@
 package approach1;
 
 public interface Subject {
-    // Both of these methods take an approach1.Observer as argument
+    // Both of these methods take an Observer as argument
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
 
-    // This method is called to notify all Observers when the approach1.Subject's
+    // This method is called to notify all Observers when the Subject's
     // state has changed
     public void notifyObservers();
 }
